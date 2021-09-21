4e1b4ccdbe7043f7a9939b8f7f3d65f670d051c4
Tom Bielawski <tfbielawski@protonmail.com>
2021-09-20 20:31:09 -0400

    adding router

---
+++ api/auth/authRouter.js
@@ -0,0 +1,26 @@
+const router = require("express").Router()
+
+//Add model functions here
+
+
+//add endpoint functions here
+router.post("/register", (req, res, next) => {
+
+})
+router.post("/register", (req, res, next) => {
+
+})
+router.post("/login", (req, res, next) => {
+
+})
+router.post("/login", (req, res, next) => {
+
+})
+router.get("/login", (req, res, next) => {
+
+})
+router.get("/login", (req, res, next) => {
+
+})
+
+module.exports = router;
