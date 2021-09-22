9707c40e1463b220fd6a88100e7213bd53353b38
Tom Bielawski <tfbielawski@protonmail.com>
2021-09-22 12:22:51 -0400

    heroku push

--- api/server.js
+++ api/server.js
@@ -28,7 +28,7 @@ server.use("/api/auth", authRouter)
 //     })
 // })
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use("*", ( err, req, res, next) => { // eslint-disable-line
     res.status(err.status || 500).json({
         message: err.message,
         stack: err.stack,
